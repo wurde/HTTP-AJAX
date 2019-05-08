eb0e5bb2cc7680f309f3711206a14a3406428157
Andy Bettisworth <andy@accreu.com>
2019-05-08 16:16:50 -0500

    Setup friends sub-app using Create-React-App

---
+++ friends/src/App.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const React = require('react')
+
+/**
+ * Import component styles
+ */
+
+require('./App.scss')
+
+/**
+ * Define component
+ */
+
+function App() {
+  return (
+    <div className="jsx-App">
+      jsx-App
+    </div>
+  )
+}
+
+/**
+ * Export component
+ */
+
+module.exports = App
